5b9ba19f637393d72e107c5b5d7bd6279b77f3f0
ninak8 <103591333+ninak8@users.noreply.github.com>
2023-06-27 00:44:28 -0300

    changes in filters

--- api/src/controllers/filters.js
+++ api/src/controllers/filters.js
@@ -46,12 +46,44 @@ const clearGet = (arr) => {
 };
 
 const filterByCategory = async (category) => {
-  const products = await Product.findAll({
-    where: {
-      category: category,
-    },
-  });
-  return products;
+  if (category === "novedades") {
+    const productsNew = await Product.findAll({
+      where: {
+        state: "nuevo",
+      },
+    });
+    return productsNew;
+  } else if (category === "deporte") {
+    const productsSports = await Product.findAll({
+      where: {
+        stock: category,
+      },
+      include: [
+        {
+          model: Tag,
+          attributes: ["name"],
+        },
+        {
+          model: Size,
+          attributes: ["size"],
+        },
+        {
+          model: Color,
+          attributes: ["name"],
+        },
+      ],
+    });
+    const clearSport = clearGet(productsSports);
+    console.log(clearSport.length);
+    return clearSport;
+  } else {
+    const products = await Product.findAll({
+      where: {
+        category: category,
+      },
+    });
+    return products;
+  }
 };
 
 const filterByScore = async () => {
@@ -82,30 +114,45 @@ const filteredByCategoryAndName = async (category, name) => {
       },
     ],
   });
-  // console.log(products);
-  // console.log(productsClear);
-  // return products;
   const productsClear = clearGet(products);
   return productsClear;
 };
 
 const filterAccessories = async (category, q) => {
-  const products = await Product.findAll({
-    where: {
-      category: category,
-    },
-    include: [
-      {
-        model: Tag,
-        attributes: ["name"],
+  if (category === "deporte") {
+    const productSport = await Product.findAll({
+      where: {
+        stock: category,
       },
-    ],
-  });
-  const clearProducts = clearDB(products);
+      include: [
+        {
+          model: Tag,
+          attributes: ["name"],
+        },
+      ],
+    });
 
-  const selectedTags = clearProducts.filter((elem) => elem.tags.includes(q));
-  console.log(selectedTags.length);
-  return selectedTags;
+    const clearProducts = clearDB(productSport);
+
+    const selectedTags = clearProducts.filter((elem) => elem.tags.includes(q));
+    return selectedTags;
+  } else {
+    const products = await Product.findAll({
+      where: {
+        category: category,
+      },
+      include: [
+        {
+          model: Tag,
+          attributes: ["name"],
+        },
+      ],
+    });
+    const clearProducts = clearDB(products);
+
+    const selectedTags = clearProducts.filter((elem) => elem.tags.includes(q));
+    return selectedTags;
+  }
 };
 module.exports = {
   filterByCategory,
